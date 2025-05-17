1150cd06c222bebbf386cc70362439a31f050266
Luis Maria Gomez <chuni12gomez@gmail.com>
2025-05-17 01:59:35 -0300

    dddd

---
+++ Distribuidora/CDatos/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Shared.Entities;
+
+namespace CDatos.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+        }
+
+        //public DbSet<Animals> Animals { get; set; } = null!; Copiar asi para entidades
+
+    }
+}
