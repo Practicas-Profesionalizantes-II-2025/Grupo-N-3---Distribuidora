1642f32fbdf6a526f4f9c5720fbe1c3fc9591f07
JuanBergonzi <juan.bergonzi.ices@gmail.com>
2025-05-17 21:30:12 -0300

    Ver Helper

---
+++ Distribuidora/CNegocio/ClientHttp/ApiServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CNegocio.Helper;
+
+namespace CNegocio.ClientHttp
+{
+    internal static class ApiServer
+    {
+        public static HttpClient ObtenerClientHttp()
+        {
+            HttpClient _client = new HttpClient();
+            _client.DefaultRequestHeaders.Add("Accept", "application/json");
+            _client.Timeout = TimeSpan.FromSeconds(15);
+
+            return _client;
+        }
+
+        internal static string ObtenerUrlEndPoint(string Path)
+        {
+            string url = ApplicationConfiguration.GetSetting("ApiServer:BaseURL") + Path;
+            return url;
+        }
+    }
+
+}
---
+++ Distribuidora/CNegocio/Helper/ApplicationConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace CNegocio.Helper
+{
+    internal class ApplicationConfiguration
+    {
+        private static IConfigurationRoot _configuration;
+
+        static ApplicationConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+          //      .SetBasePath(Directory.GetCurrentDirectory())
+          //      .AddJsonFile("negociosettings.json", optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+        }
+
+        public static string GetSetting(string key)
+        {
+            return (
+                (_configuration != null && _configuration[key] != null) ? _configuration[key]! : ""
+            );
+        }
+    }
+
+}
