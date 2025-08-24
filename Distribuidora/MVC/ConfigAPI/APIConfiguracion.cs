bd3384241519e74822eae04ce310bc0f464a86ff
Luis María Gómez <125579465+LuisMariaGomez@users.noreply.github.com>
2025-08-23 21:26:59 -0300

    listo

---
+++ Distribuidora/MVC/ConfigAPI/APIConfiguracion.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.ConfigAPI
+{
+    public class ApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string CiudadesEndpoint { get; set; }
+        public string CategoriasEndpoint { get; set; }
+        // agregá más endpoints según se necesite
+}
--- Distribuidora/MVC/Program.cs
+++ Distribuidora/MVC/Program.cs
@@ -1,9 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVC.Data;
+using MVC.ConfigAPI;
+using NuGet.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// DbContext
 builder.Services.AddDbContext<MVCContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCContext") ?? throw new InvalidOperationException("Connection string 'MVCContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCContext")
+        ?? throw new InvalidOperationException("Connection string 'MVCContext' not found.")));
+
+// Configuración de ApiSettings
+builder.Services.Configure<ApiSettings>(
+    builder.Configuration.GetSection("ApiSettings"));
+
+// Registro del HttpClient con BaseAddress
+builder.Services.AddHttpClient("API", client =>
+{
+    var baseUrl = builder.Configuration["ApiSettings:BaseUrl"];
+    client.BaseAddress = new Uri(baseUrl);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,7 +31,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
