0786fdab743be920afe489051311d675fd67090c
JuanBergonzi <juan.bergonzi.ices@gmail.com>
2025-09-23 21:48:20 -0300

    dsds

--- Distribuidora/CDatos/Data/DataContext.cs
+++ Distribuidora/CDatos/Data/DataContext.cs
@@ -29,7 +29,7 @@ namespace CDatos.Data
         public DbSet<Usuario> Usuario { get; set; } = null!;
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=SistemaGestionUniversitario;Integrated Security=True;TrustServerCertificate=true;");
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Distribuidora;Integrated Security=True;TrustServerCertificate=true;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
