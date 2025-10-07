5b5e52b4b4c90dae22a8a87e045ff829c2ecb61d
JuanBergonzi <juan.bergonzi.ices@gmail.com>
2025-10-06 22:43:03 -0300

    acomodamos en todas las vistas, ruta del js

---
+++ Distribuidora/MVC/wwwroot/js/Compra/filtroOrden.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const inputBuscar = document.getElementById("searchInput");
+
+    if (inputBuscar) {
+        inputBuscar.addEventListener("keyup", function () {
+            const valor = inputBuscar.value.toLowerCase().trim();
+            const filas = document.querySelectorAll("table tbody tr");
+
+            filas.forEach(fila => {
+                const id = fila.cells[0].textContent.toLowerCase();
+                const fecha = fila.cells[1].textContent.toLowerCase();
+                const empleado = fila.cells[2].textContent.toLowerCase();
+
+                if (valor === "" || id.includes(valor) || fecha.includes(valor) || empleado.includes(valor)) {
+                    fila.style.display = "";
+                } else {
+                    fila.style.display = "none";
+                }
+            });
+        });
+    }
+});
